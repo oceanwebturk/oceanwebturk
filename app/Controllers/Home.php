30bcb95af8c01e2f6afdaa0d65f4e616af710ec8
Mehmet Ali Durusoy <mehmetalidurusoy73+github@gmail.com>
2024-02-10 18:16:57 +0300

    Updated Files

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,11 +6,20 @@ defined("REAL_BASE_DIR") or die;
  * 
  * @package App\Controllers
  */
+use OceanWT\Http\Request;
 use OceanWT\Http\Controller;
 class Home extends Controller{
  /* index method  */
  public function index()
  {
   $this->import->view("index");
+ }
+
+ public function demo()
+ {
+  if(Request::isPost() && csrf_verify()){
+   print_r($_POST);
+  }
+  $this->import->view("form");
  }
 }
--- etc/routes.php
+++ etc/routes.php
@@ -3,3 +3,4 @@ defined("REAL_BASE_DIR") or die;
 use OceanWT\Http\Route;
 
 Route::get("/","Home::index");
+Route::match(["GET","POST"],"/demo","Home::demo",["as"=>"csrf.token.test","filters"=>["csrf"]]);
