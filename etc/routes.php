436ee2465d5cbcf01bafe855cf1eb839f6d2268c
Mehmet Ali Durusoy <mehmetalidurusoy73+github@gmail.com>
2024-02-10 18:56:00 +0300

    Updated  and deleted Files
    app/Controllers/Home.php updated
    composer.json updated
    routes.php updated
    form.php deleted

--- etc/routes.php
+++ etc/routes.php
@@ -3,4 +3,3 @@ defined("REAL_BASE_DIR") or die;
 use OceanWT\Http\Route;
 
 Route::get("/","Home::index");
-Route::match(["GET","POST"],"/demo","Home::demo",["as"=>"csrf.token.test","filters"=>["csrf"]]);
--- var/html/form.php
+++
@@ -1,6 +0,0 @@
-<?=csrf_meta()?>
-<form action="<?=route("csrf.token.test")?>" method="post">
-<input type="text" name="goolge">
-<button>Submit</button>
-</form>
-<?php print_r(get_meta_tags(REAL_BASE_DIR."public/index.php")); ?>
